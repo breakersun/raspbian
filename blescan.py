77db016e3bc10d4e2fac60021f2262a97d9994d6
Leo Sun <leosunsl@outlook.com>
2019-11-21 10:15:01 +0800

    add a simple ble scanner

---
+++ blescan.py
@@ -0,0 +1,59 @@
+import adtypes
+import sys, uuid
+from bluepy.btle import Scanner, DefaultDelegate
+
+def twos_complement(hexstr, bits):
+    value = int(hexstr,16)
+    if value & (1 << (bits-1)):
+        value -= 1 << bits
+    return value
+
+def beacon_parser(raw_data, rssi):
+    tmp = list(raw_data[0:4])
+    VID = ''.join([tmp[2], tmp[3], tmp[0], tmp[1]])
+    UUID = uuid.UUID(raw_data[4:36])
+    HW = raw_data[36:38]
+    SW = raw_data[38:40]
+    BATT = raw_data[40:42]
+    MAJOR = raw_data[42:46]
+    MINOR = raw_data[46:50]
+    RSSI_1M = raw_data[-2:]
+    return [UUID, HW, SW, twos_complement(BATT, 8), MAJOR, MINOR, RSSI_1M, rssi]
+    # print(UUID, HW, SW, BATT, MAJOR, MINOR, twos_complement(RSSI_1M, 8))
+
+class ScanDelegate(DefaultDelegate):
+    def __init__(self):
+        DefaultDelegate.__init__(self)
+
+    # def handleDiscovery(self, dev, isNewDev, isNewData):
+        # if dev.addr == dest_addr:
+        #     for (adtype, desc, value) in dev.getScanData():
+        #         if desc == 'Manufacturer':
+        #             beacon_parser(value)
+        #             print('\x1b[%dA' % (2))
+
+
+print('usage: python3 blescan blescan.py MAC_ADDR minutes\nfor example:python3 blescan.py aa:bb:cc:dd:ee:00 10')
+if len(sys.argv) != 3:
+    print('command usage error')
+    sys.exit()
+
+dest_addr = sys.argv[1]
+scan_duration = int(sys.argv[2])
+print('BT_MAC : ', dest_addr)
+print('Test Minutes : ', scan_duration)
+
+scanner = Scanner(0).withDelegate(ScanDelegate())
+
+while True:
+    try:
+        devices = scanner.scan(1)
+        for dev in devices:
+            if dev.addr == dest_addr:
+                for (adtype, desc, value) in dev.getScanData():
+                    if desc == 'Manufacturer':
+                        res = beacon_parser(value, dev.rssi)
+                        print(res)
+                        print('\x1b[%dA' % (2))
+    except:
+        scanner = Scanner(0).withDelegate(ScanDelegate())
